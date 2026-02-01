6fd7f9ab43f6c2e44289392b30e1eb16c3b843a6
Moises Abuabara <nunlamedcaf@Moisess-MacBook-Pro.local>
2026-01-31 21:38:04 -0500

    restore veil

--- GentlemanNvim/nvim/lua/plugins/colorscheme.lua
+++ GentlemanNvim/nvim/lua/plugins/colorscheme.lua
@@ -43,7 +43,7 @@ return {
       },
     },
     {
-      "mabuabaranlc/gentleman-kanagawa-blur",
+      "Gentleman-Programming/gentleman-kanagawa-blur",
       name = "gentleman-kanagawa-blur",
       priority = 1000,
     },
